81884e716b3324eb4b0293dfb8081a2f411438b5
jesuslucianop <luciano-peralta@hotmail.com>
2019-03-18 00:45:34 -0400

    Structure of Class users completed

--- Libreria/Usuarios.php
+++ Libreria/Usuarios.php
@@ -6,41 +6,44 @@ Class Usuarios
     private $password;
     private $Rol;
     private $db;
+    private $mysqli;
     private $list =   array();
     public function __Construct()
     {
    
         $this->db = Conexion::Getinstancia();
+        $this->mysqli =  $this->db->GetConexion();
     }
     public function CreateNewUser()
     {
-        $sql = "INSERT INTO usuarios (nombre,username,password,rol)
-        VALUES ('".$this->nombre."')";
+        $data = $this->mysqli->query("INSERT INTO usuarios (nombre,username,password,rol) VALUES ('".$nombre."' '".$username."','".$password."','".$rol."')");
+
     }
+    // Method for Get all users from database 
     public  function GetallUsuarios()
     {
         $sql = "Select * from usuarios";
-        $mysqli = $this->db->GetConexion();
-       
-         $data= $mysqli->query("select * from usuarios");
-        $fila = $data->fetch_object();
-        /*foreach ($fila as $value) {
-           // echo "$value <br>";
-            echo json_encode($value);
-        }*/
-        while ($obj = $data->fetch_object()) {
-            printf ("%s (%s)\n", $obj->Nombre, $obj->Username);
+         $data= $this->mysqli->query("select * from usuarios order by id asc");
+        $res = $data->fetch_row();
+        if($data){
+        while($row = $data->fetch_array())
+        {
+        echo json_encode($row); 
+        echo "<br />";
         }
-     
     }
-    public function Updatenew()
+     
+    
+    }
+    public function UpdateUser()
     {
-
+        $data = $this->mysqli->query("UPDATE usuarios SET id = '".$id."',Nombre = '".$Nombre."',Username = '".$Username."', Password = '".$Password."',Rol = '".$rol."' WHERE id = '".$id."' ");
 
     }
     public function DeleteUser()
     {
-        
+        $data = $this->mysqli->query("Delete from usuarios where Username = '".$username."' ");
+
     }
 
 }
